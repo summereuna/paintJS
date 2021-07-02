e70a82f076a6b95cb4689ba0f9e69cf31d01a024
summereuna <summereuna@gmail.com>
2021-07-02 21:44:57 +0900

    Add color change event

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName("jsColor");
+
 
 //pixel modifier에도 canvas사이즈 만큰 사이즈 줘야 그림 그려짐
 canvas.width = 500;
@@ -38,10 +40,10 @@ function onMouseMove(event){
 
 }
 
-function onMouseDown(event){
-    painting = true;
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
-
 
 if (canvas) {
    //캔버스 위에 마우스가 있으면 감지하는 이벤트
@@ -53,3 +55,7 @@ if (canvas) {
     //마우스 떠나면 페인팅 안되게하는 이벤트
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => 
+    color.addEventListener("click", handleColorClick)
+    );
